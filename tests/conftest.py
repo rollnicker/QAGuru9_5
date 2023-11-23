a9c3035d5b6e8684b589252a06ba6e2576487e60
Rolnik Boris <rollnicker@mail.ru>
2023-11-24 00:14:07 +0300

    Автотест на demoqa создана папка tests Не выполнено: загрузка картинки

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,8 @@ from selenium import webdriver
 @pytest.fixture(scope='function', autouse=True)
 def browser_open():
     #browser.config.base_url = "https://demoqa.com/"
+    driver_options = webdriver.ChromeOptions()
+    driver_options.add_argument('--headless')
     browser.config.window_height = 3000
     browser.config.window_width = 1400
     browser.config.timeout = 4.0
